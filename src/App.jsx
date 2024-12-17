432332f5a3e23a8875dc66c84fb8d65c12f54346
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-12-17 00:48:58 -0600

    wave 1 implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css';
+import messagesData from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={messagesData[0].sender}
+          body={messagesData[0].body}
+          timeStamp={messagesData[0].timeStamp}>
+        </ChatEntry>
       </main>
     </div>
   );
